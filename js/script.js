363231616672478c31cea23e1f9f99d63c90aef2
Rina Caladis <119719820+RinaCLDS@users.noreply.github.com>
2023-07-02 22:32:26 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
 
-function show1(){
+function show1() {
     document.getElementById('vid-container').style.display = "grid";
     document.getElementById('PhotoG').style.display = "none";
     document.getElementById('cover').style.display = "none";
@@ -9,7 +9,7 @@ function show1(){
     document.getElementById('heading').textContent = "Video clips";
 }
 
-function show2(){
+function show2() {
     document.getElementById('PhotoG').style.display = "grid";
     document.getElementById('vid-container').style.display = "none";
     document.getElementById('cover').style.display = "none";
@@ -17,9 +17,9 @@ function show2(){
     document.getElementById('heading').textContent = "Photos";
 }
 
-var myVideo = document.getElementById('video'); 
+var myVideo = document.getElementById('video');
 
-function hide1(){
+function hide1() {
     document.getElementById('cover').style.display = "block";
     document.getElementById('vid-container').style.display = "none";
     document.getElementById('PhotoG').style.display = "none";
@@ -29,27 +29,27 @@ function hide1(){
 }
 
 
-$ = function(id) {
+$ = function (id) {
     return document.getElementById(id);
-  }
-var show = function(id) {
-	$(id).style.display ='block';
 }
-var hide = function(id) {
-	$(id).style.display ='none';
+var show = function (id) {
+    $(id).style.display = 'block';
+}
+var hide = function (id) {
+    $(id).style.display = 'none';
 }
 
 let loginForm = document.querySelector('.header .login-form');
 
-document.querySelector('#login-btn').onclick = () =>{
+document.querySelector('#login-btn').onclick = () => {
     loginForm.classList.toggle('active');
     navbar.classList.remove('active');
 }
 
 let navbar = document.querySelector('.header .navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');    
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
     loginForm.classList.remove('active');
 }
 
@@ -57,11 +57,11 @@ let listVideo = document.querySelectorAll('.video-list .vid');
 let mainVideo = document.querySelector('.main-video video');
 let title = document.querySelector('.main-video .title');
 
-listVideo.forEach(video =>{
-    video.onclick = () =>{
+listVideo.forEach(video => {
+    video.onclick = () => {
         listVideo.forEach(vid => vid.classList.remove('active'));
         video.classList.add('active');
-        if(video.classList.contains('active')){
+        if (video.classList.contains('active')) {
             let src = video.children[0].getAttribute('src');
             mainVideo.src = src;
             let text = video.children[1].innerHTML;
@@ -71,21 +71,21 @@ listVideo.forEach(video =>{
 })
 
 
-window.onscroll = () =>{
+window.onscroll = () => {
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
 
-    if(window.scrollY > 0){
+    if (window.scrollY > 0) {
         document.querySelector('.header').classList.add('active');
-    }else{
+    } else {
         document.querySelector('.header').classList.remove('active');
     }
 }
 
-window.onload = () =>{
-    if(window.scrollY > 0){
+window.onload = () => {
+    if (window.scrollY > 0) {
         document.querySelector('.header').classList.add('active');
-    }else{
+    } else {
         document.querySelector('.header').classList.remove('active');
     }
 }
@@ -95,15 +95,30 @@ var imgContainer = document.getElementsByClassName('.img-container');
 var images = imgContainer.querySelectorAll('img');
 
 for (var i = 0; i < images.length; i++) {
-  images[i].addEventListener('click', function() {
-    var modal = document.createElement('div');
-    modal.classList.add('modal');
-    modal.innerHTML = '<img src="' + this.src + '">';
-    modal.addEventListener('click', function() {
-      this.remove();
+    images[i].addEventListener('click', function () {
+        var modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.innerHTML = '<img src="' + this.src + '">';
+        modal.addEventListener('click', function () {
+            this.remove();
+        });
+        document.body.appendChild(modal);
     });
-    document.body.appendChild(modal);
-  });
 }
 
 
+// Show the announcement
+function showAnnouncement() {
+    document.getElementById('announcement').style.display = "block";
+}
+
+// Hide the announcement
+function hideAnnouncement() {
+    document.getElementById('announcement').style.display = "none";
+}
+
+// Add event listener to the close button
+document.querySelector('.close').addEventListener('click', hideAnnouncement);
+
+// Show the announcement once the website is opened
+window.addEventListener('DOMContentLoaded', showAnnouncement);
